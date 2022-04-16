06b3b158578199c9ca7835483653f3e0b7361c8c
Albenis <67173990+apalarcon@users.noreply.github.com>
2022-04-17 01:10:02 +0200

    Add files via upload

---
+++ run-HuMPI.py
@@ -0,0 +1,12 @@
+import numpy as np
+import humpi
+
+args = humpi.read_args()
+if args.HuMPI_help:
+	humpi.help()
+elif args.get_template:
+	humpi.get_HuMPI_inputs_template()
+elif args.get_input_data:
+	humpi.get_HuMPI_input_data()
+else:
+	humpi.HuMPI_main(args.parameterfile) 
